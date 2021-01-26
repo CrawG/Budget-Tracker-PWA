fd5bde8ddf4e908db6eab8e2451c611ee68bf6ec
Crawford Smith <crawfordgsmith@gmail.com>
2021-01-25 20:32:36 -0500

    webpack functional

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  mode: "production",
+  entry: {
+    index: "./public/index.js"
+  },
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "[name].bundle.js"
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      filename: "manifest.json",
+      inject: false,
+      fingerprints: false,
+      name: "Budget Tracker PWA",
+      short_name: "Budget",
+      theme_color: "#e6f542",
+      background_color: "#e6f542",
+      start_url: "/",
+      display: "fullscreen",
+      orientation: "landscape",
+      icons: [
+        {
+          src: path.resolve(
+            __dirname,
+            "public/icons/icon-512x512.png"
+          ),
+          size: [72, 96, 128, 144, 152, 192, 384, 512]
+        }
+      ]
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  
+  devServer: {
+    contentBase: './public',
+    publicPath: '/dist'
+ }
+};
+
+module.exports = config;
