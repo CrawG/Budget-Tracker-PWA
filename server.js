6598f4ef004047558cb77056284f6436d7e5d867
Crawford Smith <crawfordgsmith@gmail.com>
2021-01-27 17:51:41 -0500

    fixing heroku connection issues

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static("public"));
 //   useNewUrlParser: true,
 //   useFindAndModify: false
 // });
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/UNC-Bootcamp", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/UNC-BootcampHW2", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
